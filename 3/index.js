6bcb376bd25f2043f8f1c38a44a070e87dc0fed7
Tony Wooster <twooster@gmail.com>
2020-12-04 13:15:32 +0100

    cleaner day 3 one liner

--- 3/index.js
+++ 3/index.js
@@ -1,1 +1,18 @@
-console.log( ((I, C, A) => A.map(S => S.map(s => C(I, s)).reduce((a, v) => a * v, 1)))( require('fs').readFileSync('./input', 'utf8').split('\n').filter(x => x), (s, [dx, dy]) => s.reduce(([c, x, ry], l) => ry > 1 ? [c, x, ry - 1] : [c + (l[x] === '#' & 1), (x + dx) % l.length, dy], [0, 0, 0])[0], [ [[3, 1]], [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]] ]))
+console.log(
+  (I =>
+    [
+      [[3, 1]],
+      [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    ].map(s =>
+      s.map(([dx, dy]) =>
+        I.reduce(
+          ([c, x, ry], l) =>
+            ry > 1
+             ? [c, x, ry - 1]
+             : [c + (l[x] === '#' & 1), (x + dx) % l.length, dy],
+          [0, 0, 0]
+        )[0]
+      ).reduce((a, v) => a * v)
+    )
+  )(require('fs').readFileSync('./input', 'utf8').split('\n').filter(x => x))
+)
