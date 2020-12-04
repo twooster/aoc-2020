79453036202292decb16ee6b761b926fa1a2b92d
Tony Wooster <twooster@gmail.com>
2020-12-04 13:28:27 +0100

    day 2 to one liner

--- 2/index.js
+++ 2/index.js
@@ -1,30 +1,12 @@
-const fs = require('fs')
-
-const input = fs.readFileSync('./input', 'utf8')
-
-const passwords = input.split('\n').filter(x => x)
-  .map(x => /^(?<min>\d+)-(?<max>\d+) (?<char>.): (?<password>.*)$/.exec(x).groups)
-  .map(x => ({ ...x, min: parseInt(x.min, 10), max: parseInt(x.max, 10) }))
-
-let valid = 0
-for (const { char, min, max, password }  of passwords) {
-  let count = 0
-  for (const c of password) {
-    if (c === char) {
-      ++count
-    }
-  }
-  if (count >= min && count <= max) {
-    ++valid
-  }
-}
-
-console.log(valid)
-
-valid = 0
-for (const { char, min, max, password } of passwords) {
-  if ((password[min-1] === char) ^ (password[max-1] === char)) {
-    ++valid
-  }
-}
-console.log(valid)
+console.log(
+  require('fs').readFileSync('./input', 'utf8').split('\n').filter(x => x)
+    .map(x => /^(?<m>\d+)-(?<M>\d+) (?<C>.): (?<p>.*)$/.exec(x).groups)
+    .map(({ m, M, ...x }) => ({ ...x, m: parseInt(m, 10), M: parseInt(M, 10) }))
+    .reduce(([c1, c2], { m, M, C, p }) =>
+    [
+      (c => c1 + ((c >= m && c <= M) & 1))(Array.from(p).reduce((a, c) => a + (c === C & 1), 0)),
+      c2 + ((p[m-1] === C) ^ (p[M-1] === C) & 1),
+    ],
+    [0, 0]
+  )
+)
