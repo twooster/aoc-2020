ed2631aced24ef3c96efadd8bfb2f00fd847f0ca
Tony Wooster <twooster@gmail.com>
2020-12-04 10:02:38 +0100

    day 3 one liner

--- 3/index.js
+++ 3/index.js
@@ -1,10 +1,1 @@
-const lines = require('fs').readFileSync('./input', 'utf8').split('\n').filter(x => x)
-const count = (slope, [dx, dy], tree = '#') =>
-  slope.reduce(([c, x, ry], l) =>
-    ry > 1
-     ? [c, x, ry - 1]
-     : [l[x] === tree ? c + 1 : c, (x + dx) % l.length, dy],
-  [0, 0, 0])[0]
-
-console.log(count(lines, [3, 1]))
-console.log([[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]].map(step => count(lines, step)).reduce((a, v) => a * v, 1))
+console.log( ((I, C, A) => A.map(S => S.map(s => C(I, s)).reduce((a, v) => a * v, 1)))( require('fs').readFileSync('./input', 'utf8').split('\n').filter(x => x), (s, [dx, dy]) => s.reduce(([c, x, ry], l) => ry > 1 ? [c, x, ry - 1] : [c + (l[x] === '#' & 1), (x + dx) % l.length, dy], [0, 0, 0])[0], [ [[3, 1]], [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]] ]))
