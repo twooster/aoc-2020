2c5841e3cedd9a97b2ad7f81fc84c4bad4d69624
Tony Wooster <twooster@gmail.com>
2020-12-05 10:47:32 +0100

    more confusing!

--- 5/index.js
+++ 5/index.js
@@ -1,14 +1,14 @@
 console.log(
-  require('fs').readFileSync('./input', 'utf8').split('\n').filter(x => x).map(s =>
-    Buffer.from(s).reduce((a, c) =>
-      !(c & 12) | a << 1,
-      0
-    )
-  ).sort((a, b) => a - b).reduce(([x, m], v, i, a) =>
+  require('fs').readFileSync('./input').reduce(
+    ([l, a], c) => c === 10
+      ? [[...l, a], 0]
+      : [l, !(c & 12) | a << 1],
+    [[], 0]
+  )[0].sort((a, b) => a - b).reduce(([x, m], v, i, a) =>
     [
       v,
       m || v+1 === a[i+1] ? m : v+1
     ],
-    [0, 0]
+    []
   )
 )
