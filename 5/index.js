13e9667c85a20b170a6537f18192408cbdf4cdca
Tony Wooster <twooster@gmail.com>
2020-12-05 10:30:25 +0100

    even shorter

--- 5/index.js
+++ 5/index.js
@@ -1,13 +1,13 @@
 console.log(
   require('fs').readFileSync('./input', 'utf8').split('\n').filter(x => x).map(s =>
-    Array.from(s).reduce((a, c) =>
-      a << 1 | (c === 'B' || c === 'R' & 1),
+    Buffer.from(s).reduce((a, c) =>
+      !(c & 12) | a << 1,
       0
-    ),
+    )
   ).sort((a, b) => a - b).reduce(([x, m], v, i, a) =>
     [
       v,
-      m === 0 && v+1 !== a[i+1] ? v+1 : m
+      m || v+1 === a[i+1] ? m : v+1
     ],
     [0, 0]
   )
