2bf4391cf7f90900542a075284f559b24d09a71c
Tony Wooster <twooster@gmail.com>
2020-12-06 20:05:42 +0100

    iterating

--- 6/index.js
+++ 6/index.js
@@ -1,15 +1,15 @@
 console.log(
-  (([l, b, a, x]) => ([...l, [a, b]]))(require('fs').readFileSync('./input').reduce(
-    ([l, b, a, x, s], c) => c === 10
-      ? s
+  require('fs').readFileSync('./input').reduce(
+    ([l, b, a, x, s], c, i, u) => c === 10
+      ? s || i+1 === u.length
         ? [[...l, [a, b]], ~0]
         : [l, b & x, a | x, , 1]
       : [l, b, a, 1 << (c-97) | x],
     [[], ~0]
-  )).map(x =>
+  )[0].map(x =>
     x.map(y =>
       // Shorter:
-      Array(26).fill(0).reduce((a, _, i) => a + !!((1 << i) & y), 0)
+      Array(26).fill().reduce((a, _, i) => a + !!(1 << i & y), 0)
       // Bit-bangy:
       /*
       (c => (((c + (c >> 4)) & 0xF0F0F0F) * 0x1010101) >> 24)(
