389eddac2a8c7d224d4515caf3bfa937b41ff55d
Tony Wooster <twooster@gmail.com>
2020-12-06 17:34:40 +0100

    day 6 part 1

--- 5/index.js
+++ 5/index.js
@@ -1,9 +1,9 @@
 console.log(
   require('fs').readFileSync('./input').reduce(
     ([l, a], c) => c === 10
-      ? [[...l, a], 0]
+      ? [[...l, a]]
       : [l, !(c & 12) | a << 1],
-    [[], 0]
+    [[]]
   )[0].sort((a, b) => a - b).reduce(([x, m], v, i, a) =>
     [
       v,
