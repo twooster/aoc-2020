be5c66341d65b99a84517550508120635e4e1d4b
Tony Wooster <twooster@gmail.com>
2020-12-07 22:17:50 +0100

    dumb day 7

--- 7/index.js
+++ 7/index.js
@@ -1,44 +1,57 @@
-const bags = require('fs').readFileSync('./input', 'utf8').split('\n')
-  .map(s => s.split(/ bags?(?:, | contain (?:no other bags\.)?|\.)/).slice(0, -1))
-  .reduce(([P, C], [p, ...t]) =>
-    (C[p] = t.map(s => /(\d+) (.*)/.exec(s))
-      .map(([,n,c]) => [c, +n])
-      .map(([c, n]) => (P[c] = [...(P[c] || []), p], [c, n])), [P, C]),
-    [{}, {}]
-  )
+// I couldn't find a way to oneline this that didn't
+// make me go insane. Sigh.
+const bags =
+    require('fs').readFileSync('./input', 'utf8').split('\n').filter(x => x)
+      .map(s => s.split(/ bags?(?:, | contain (?:no other bags\.)?|\.)/).slice(0, -1))
+      .map(([h, ...t]) =>
+        [h, t.map(
+          s => (([, c, n]) => [n, +c])(/(\d+) (.*)/.exec(s))
+        )])
 
-console.log(bags[0])
-console.log(bags[1])
 
-const [parents, children] = bags
-let search = parents['shiny gold']
-let found = new Set()
-while (search.length) {
-  const [name, c] = search.pop()
-  if (!found.has(name)) {
-    found.add(name)
-    search.push(...parents[name] || [])
-  }
+const bagMap = new Map()
+for (const [b] of bags) {
+  bagMap.set(b, { parents: [] })
 }
-console.log(found.size)
-/*
-const z = require('fs').readFileSync('./input', 'utf8').split('\n')
-  .map(s => (s.split(/ bags?(?:, | contain (?:no other bags\.)?|\.)/).slice(0, -1)))
-  .reduce((a, [p, ...t]) =>
-    (t.map(s => /(\d+) (.*)/.exec(s))
-     .forEach(([,n,c]) => a[c] = [...(a[c] || []), p]), a),
-    {}
-  )
+for (const [parentName, children] of bags) {
+  const parent = bagMap.get(parentName)
+  parent.children = children.map(([childName, count]) => {
+    const child = bagMap.get(childName)
+    child.parents.push(parent)
+    return { child, count }
+  })
+}
 
-const z = require('fs').readFileSync('./input', 'utf8').split('\n').map(
-  s => (s.split(/ bags?(?:, | contain (?:no other bags)?|\.)/).slice(0, -1))
-).map(([h, ...t]) =>
-  t.map(s => /(\d+) (.*)/.exec(s))
-   .reduce((l, [,n,b]) => [...l, [b, [h, +n]]], [])
-).reduce((a, l) =>
+const shinyGold = bagMap.get('shiny gold')
 
-  (l.forEach(([c, [p]]) => (a[c] = [...a[c] || [], p])), a), {}
+console.log(
+  (work => {
+    const seen = new Set()
+    let count = 0
+    while (work.length) {
+      for (const parent of work.pop()) {
+        if (!seen.has(parent)) {
+          seen.add(parent)
+          work.push(parent.parents)
+          ++count
+        }
+      }
+    }
+    return count
+  })([shinyGold.parents])
 )
 
-console.log(JSON.stringify(z, null, 2))
-*/
+console.log(
+  (work => {
+    let total = 0
+    while (work.length) {
+      const [mul, children] = work.pop()
+      for (const { child, count } of children) {
+        const n = count * mul
+        total += n
+        work.push([n, child.children])
+      }
+    }
+    return total
+  })([[1, shinyGold.children]])
+)
