253d520d7f2cba06355e2d01ccca085797166bad
Tony Wooster <twooster@gmail.com>
2020-12-07 20:39:42 +0100

    iterating

--- 7/index.js
+++ 7/index.js
@@ -1,18 +1,23 @@
 const bags = require('fs').readFileSync('./input', 'utf8').split('\n')
   .map(s => s.split(/ bags?(?:, | contain (?:no other bags\.)?|\.)/).slice(0, -1))
-  .reduce((a, [p, ...t]) =>
-    (t.map(s => /(\d+) (.*)/.exec(s))
-     .forEach(([,n,c]) => a[c] = [...(a[c] || []), [p, +n]]), a),
-    {}
+  .reduce(([P, C], [p, ...t]) =>
+    (C[p] = t.map(s => /(\d+) (.*)/.exec(s))
+      .map(([,n,c]) => [c, +n])
+      .map(([c, n]) => (P[c] = [...(P[c] || []), p], [c, n])), [P, C]),
+    [{}, {}]
   )
 
-let search = bags['shiny gold']
+console.log(bags[0])
+console.log(bags[1])
+
+const [parents, children] = bags
+let search = parents['shiny gold']
 let found = new Set()
 while (search.length) {
   const [name, c] = search.pop()
   if (!found.has(name)) {
     found.add(name)
-    search.push(...bags[name] || [])
+    search.push(...parents[name] || [])
   }
 }
 console.log(found.size)
