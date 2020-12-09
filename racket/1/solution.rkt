23a5ad1208124468f18ca97aaee60d1ae9045d7f
Tony Wooster <twooster@gmail.com>
2020-12-09 13:45:29 +0100

    add faster racket day 1

--- racket/1/solution.rkt
+++ racket/1/solution.rkt
@@ -4,6 +4,24 @@
 
 (define numbers (map string->number (read-lines "./input")))
 
+; faster
+(define (first-combination-eq lst n sum)
+  (if (empty? lst)
+    #f
+    (if (eq? n 1)
+      (let ([found (findf (curry eq? sum) lst)])
+        (and found (list found)))
+      (let* ([h (first lst)]
+             [t (rest lst)]
+             [found (first-combination-eq t (sub1 n) (- sum h))])
+        (if found
+          (cons h found)
+          (first-combination-eq t n sum))))))
+
+(displayln (apply * (first-combination-eq numbers 2 2020)))
+(displayln (apply * (first-combination-eq numbers 3 2020)))
+
+;smaller
 (define (first-combination-where fn lst n)
   (for/first ([l (in-combinations lst n)]
               #:when (fn l))
