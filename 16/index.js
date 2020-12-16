9224155c5c1e5587d933ac8720be806bd33098fd
Tony Wooster <twooster@gmail.com>
2020-12-16 20:46:22 +0100

    day 16 part 2

--- 16/index.js
+++ 16/index.js
@@ -28,23 +28,57 @@ const computeValidity = t => t.map(v =>
 
 const validity = otherTickets.map(computeValidity)
 
-const [validValidity, errSum] =
-  validity.reduce(([vv, es], vals) => {
-    const e = vals.reduce((a, [v, validity]) =>
-        a + (validity.includes(true) ? 0 : v),
+const [validTickets, errSum] =
+  validity.reduce(([vt, es], vals) => {
+    const e = vals.reduce((a, [v, fieldValidity]) =>
+        a + (fieldValidity.includes(true) ? 0 : v),
         0)
     if (e === 0) {
-      vv.push(vals)
+      vt.push(vals)
     }
-    return [vv, es + e]
+    return [vt, es + e]
   },
   [[], 0])
 
 console.log(errSum)
 
-const fullValidity = [computeValidity(myTicket), ...validValidity]
+//  tickets -> fields -> rule checks
+const allValidTickets = [computeValidity(myTicket), ...validTickets]
+  .map(x => x.map(x => x[1])) // drop field values
 
-const convolve = a => a[0].map((_, i) => a.map(bx => bx[i]))
-const fieldValidity = convolve(fullValidity.map(x => x.map(x => x[1])))
+const validRuleForField = new Map(allValidTickets[0].map((r, i) => [i, new Set(r.map((_, i) => i))]))
+allValidTickets.forEach(fields =>
+  fields.forEach((rules, i) =>
+    rules.forEach((rule, j) => rule || validRuleForField.get(i).delete(j))))
+
+const ruleToField = new Map()
+while (validRuleForField.size) {
+  let foundRule
+  for (const [fieldId, rules] of validRuleForField) {
+    if (rules.size === 1) {
+      foundRule = Array.from(rules)[0]
+      ruleToField.set(foundRule, fieldId)
+      validRuleForField.delete(fieldId)
+      break
+    }
+  }
+  for (const [fieldId, rules] of validRuleForField) {
+    rules.delete(foundRule)
+  }
+}
+
+console.log(rules
+  .map(([name], i) => [name, i])
+  .filter(([name]) => name.startsWith('departure'))
+  .map(([, i]) => i)
+  .reduce((acc, ruleId) => acc * myTicket[ruleToField.get(ruleId)], 1)
+)
 
 
+//const rotate2dMatrix = a => a[0].map((_, i) => a.map(bx => bx[i]))
+
+// const fieldToReducedRuleChecks =
+//   rotate2dMatrix(
+//     rotate2dMatrix(allValidTickets).map(x =>
+//       x.reduce((acc, y) => y.map((v, i) => v && acc[i])))
+//   ).map((a, i) => [i, a.reduce((acc, v) => acc + (v & 1)), a])
