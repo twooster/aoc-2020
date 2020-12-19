24fdb412c6fac57ff9396cb79f7bbc04829cfa25
Tony Wooster <twooster@gmail.com>
2020-12-19 09:04:56 +0100

    simplify more

--- 19/index.js
+++ 19/index.js
@@ -1,57 +1,42 @@
 const [rules, inputs] = require('fs').readFileSync('./input', 'utf8').split('\n\n').map(x => x.split('\n').filter(y => y))
 
-const rulesPart1 = new Map(rules.map(r => r.split(': ')))
-const rulesPart2 = new Map(rulesPart1)
-  .set('8', '42 | 42 8')
-  .set('11', '42 31 | 42 11 31')
-
 const TERMINAL = /^"([^"]+)"$/
 function makeParser(rules) {
-  const memo = new Map()
-  const parse = k => {
-    let fn = memo.get(k)
-    if (fn) {
-      return fn
-    }
+  const parsers = new Map()
 
-    const v = rules.get(k)
-    if (v === undefined) {
-      throw new Error(`oh no ${k}`)
-    }
-
+  for (const [k, v] of rules) {
     const term = v.match(TERMINAL)
     if (term) {
       const t = term[1]
-      return function * (s, i) {
+      parsers.set(k, function * (s, i) {
         if (s.slice(i, i + t.length) === t) {
           yield i + t.length
         }
-      }
-    }
+      })
+    } else {
+      const parts = v.split('|').map(x => x.split(/\s+/).filter(x => x))
 
-    const parts = v.split('|').map(x => x.split(/\s+/).filter(x => x))
-
-    fn = function * (s, i = 0) {
-      for (const pieces of parts) {
-        const pf = pieces.map(p => parse(p))
-        const recur = function * (pi, i) {
-          const f = pf[pi]
-          if (!f) {
-            yield i
-          } else {
-            for (const j of f(s, i)) {
-              yield * recur(pi + 1, j)
+      parsers.set(k, function * (s, i = 0) {
+        for (const pieces of parts) {
+          const pf = pieces.map(p => parsers.get(p))
+          const recur = function * (pi, i) {
+            const f = pf[pi]
+            if (!f) {
+              yield i
+            } else {
+              for (const j of f(s, i)) {
+                yield * recur(pi + 1, j)
+              }
             }
           }
+          yield * recur(0, i)
         }
-        yield * recur(0, i)
-      }
+      })
     }
-    memo.set(k, fn)
-    return fn
   }
+
   return r => s => {
-    for (const j of parse(r)(s)) {
+    for (const j of parsers.get(r)(s)) {
       if (j === s.length) {
         return true
       }
@@ -60,8 +45,14 @@ function makeParser(rules) {
   }
 }
 
+const rulesPart1 = new Map(rules.map(r => r.split(': ')))
+const rulesPart2 = new Map(rulesPart1)
+  .set('8', '42 | 42 8')
+  .set('11', '42 31 | 42 11 31')
+
 const part1 = makeParser(rulesPart1)('0')
 console.log(inputs.reduce((acc, i) => acc + (part1(i) & 1), 0))
+
 const part2 = makeParser(rulesPart2)('0')
 console.log(inputs.reduce((acc, i) => acc + (part2(i) & 1), 0))
 
