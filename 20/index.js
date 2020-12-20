c13afc024762059d22df8b943d9d481e5e30fdd4
Tony Wooster <twooster@gmail.com>
2020-12-20 13:29:49 +0100

    iterating

--- 20/index.js
+++ 20/index.js
@@ -2,37 +2,58 @@ const T = 0
 const R = 1
 const B = 2
 const L = 3
+const TILE = 4
+
+const fromBin = (acc, d) => (acc << 1) | d
+const computeTile = t => ([
+  t[0].reduce(fromBin, 0),
+  t.map(l => l[l.length - 1]).reduce(fromBin, 0),
+  t[t.length - 1].reduce(fromBin, 0),
+  t.map(l => l[0]).reduce(fromBin, 0),
+  t
+])
+const rotateClockwise =
+  l => l.map((row, y) => row.map((_, x) => l[row.length - x - 1][y]))
+const flipVert = a => a.map((_, y) => a[a.length - y - 1])
+// a b c
+// d e f
+// g h i
+
+
+/*
+const a = ['abc'.split(''), 'hid'.split(''), 'gfe'.split('')]
+console.log(a.map(x => x.join('')).join('\n'))
+console.log()
+console.log(rotateClockwise(a).map(x => x.join('')).join('\n'))
+console.log()
+console.log(rotateClockwise(rotateClockwise(a)).map(x => x.join('')).join('\n'))
+console.log()
+console.log(rotateClockwise(rotateClockwise(rotateClockwise(a))).map(x => x.join('')).join('\n'))
+*/
 
 const input = new Map(require('fs').readFileSync('./input', 'utf8').split('\n\n')
   .map(s => s.match(/^Tile (\d+):\n(.+)\n?$/sm))
   .map(([,id,lines]) => {
     id = parseInt(id, 10)
-    lines = lines.replace(/\./g, '0').replace(/#/g, '1').split('\n').filter(x => x)
+    lines = lines.split('\n').filter(x => x).map(s => s.split('').map(x => x === '.' ? 0 : 1))
 
-    const a = lines[0].split('')
-    const b = lines.map(l => l[l.length - 1])
-    const c = lines[lines.length - 1].split('').reverse()
-    const d = lines.map(l => l[0]).reverse()
+    const permutations = []
 
-    const edgeValues = [a, b, c, d]
-      .map(e => [parseInt(e.join(''), 2), parseInt(Array.from(e).reverse().join(''), 2)])
-
-    const permutations = Array.from(permutationsOf(edgeValues))
+    let last = lines
+    let lastMirror = flipVert(lines)
+    for (let i = 0; true; ++i) {
+      permutations.push(computeTile(last))
+      permutations.push(computeTile(lastMirror))
+      if (i < 3) {
+        last = rotateClockwise(last)
+        lastMirror = rotateClockwise(lastMirror)
+      } else {
+        break
+      }
+    }
     return [id, permutations]
   }))
 
-function * permutationsOf (e) {
-  for (let i = 0; i < 4; ++i) {
-    const [t, r, b, l] = [
-      e[(0 + i)%4][0],
-      e[(1 + i)%4][0],
-      e[(2 + i)%4][1],
-      e[(3 + i)%4][1],
-    ]
-    yield [t, r, b, l]
-    yield [l, b, r, t]
-  }
-}
 
 function findSquarePlacements(input, side) {
   function recur(arr, unused, l, t) {
@@ -46,8 +67,6 @@ function findSquarePlacements(input, side) {
       const nextAtTopEdge = (arr.length + 1) >= side
       const nextAtLeftEdge = (arr.length + 1) % side === 0
       const nt = nextAtTopEdge ? arr[arr.length - side + 1][1][B] : undefined
-      // side === 3
-      // [1, 2, 3]
       for (const perm of matchPerms) {
         arr.push([id, perm])
         const nl = nextAtLeftEdge ? undefined : perm[R]
@@ -63,15 +82,34 @@ function findSquarePlacements(input, side) {
   return recur([], new Set(input.keys()))
 }
 
-function part1(input) {
+function findAllTheAnswersToAllTheThings(input) {
   const side = Math.sqrt(input.size)
   if (!Number.isInteger(side)) {
     throw new Error('oh no')
   }
   const corners = [0, side - 1, input.size - 1, input.size - side]
-  return findSquarePlacements(input, side)
+
+  const placements = findSquarePlacements(input, side)
+  console.log('placements', placements)
+
+  const part1Answer = placements
     .filter((_, i) => corners.includes(i))
     .map(([id]) => id)
+    .reduce((a, b) => a * b, 1)
+
 }
 
-console.log(part1(input).reduce((a, b) => a * b, 1))
+
+
+/*
+01234567890123456789
+..................#
+#....##....##....###
+.#..#..#..#..#..#
+*/
+// return b[o + 0] === 1 && b[o + 5] === 1 && b[o + 6] === 1 && b[o + 11] === 1 && b[o + 12] === 1 && b[o + 17] === 1 && b[o + 18] && b[o + 19] === 1
+//   && c[o + 1] === 1 && c[o + 4] === 1 && c[o + 7] === 1 && c[o + 10] === 1 && c[o + 13] === 1 && c[o + 16] === 1
+//   && a[o + 18] === 1
+//
+
+findAllTheAnswersToAllTheThings(input)
